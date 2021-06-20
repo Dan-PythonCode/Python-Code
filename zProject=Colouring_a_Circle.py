f95925b0575b32315ece371edd7690b66f1b3709
Dan-PythonCode <85842134+Dan-PythonCode@users.noreply.github.com>
2021-06-20 11:35:15 -0600

    Add files via upload

---
+++ zProject=Colouring_a_Circle.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.fillcolor("orange")
+turtle.begin_fill()
+turtle.end_fill()
+repeats = 0
+while repeats < 360:
+    turtle.forward(1)
+    turtle.right(1)
+    repeats = repeats + 1
+
+
